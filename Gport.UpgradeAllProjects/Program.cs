787bcbbb24c46372c174a132b1910a3d0f764d0f
gport <garport96@gmail.com>
2024-05-20 08:54:34 -0700

    Revert "add net 8 conversion"
    This reverts commit 0ac485b8e12cc731e2af3011a7c662bc37f3a6a5.

--- Gport.UpgradeAllProjects/Program.cs
+++ Gport.UpgradeAllProjects/Program.cs
@@ -20,26 +20,6 @@ var root = new RootCommand
     Name = "upgrade-all-projects"
 };
 var path = new Option<DirectoryInfo>("--sourceDirectory", "The toplevel directory to search for .csproj files");
-var net8 = new Command("net8"); 
-net8.AddOption(path);
-net8.SetHandler(async sourceDirectory => {
-    var matchingFiles = Directory.EnumerateFiles(sourceDirectory.FullName, "*.csproj", SearchOption.AllDirectories)
-                                 .ToArray();
-    foreach (var file in matchingFiles)
-    {
-        Console.WriteLine(file);
-    }
-    Console.WriteLine("Identified {0} C# projects", matchingFiles.Length);
-    Console.Write("Would you like to attempt to upgrade all projects to .NET 8? (Y/N): ");
-    var confirm = Console.ReadLine();
-    if (String.Equals(confirm?.ToUpperInvariant(), "Y", StringComparison.OrdinalIgnoreCase))
-    {
-        foreach (var file in matchingFiles)
-        {
-            await Process.Start("upgrade-assistant", new[] { "upgrade", $"{file}", $"--operation", "framework.inplace", $"--targetFramework", "net8.0", "--non-interactive" }).WaitForExitAsync();
-        }
-    }
-}, path);
 var sdk = new Command("sdk");
 sdk.AddOption(path);
 sdk.SetHandler(async sourceDirectory => {
